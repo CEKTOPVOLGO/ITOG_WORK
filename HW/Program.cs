c29331c82acb7c76c8978639c9d4e7d05a3ec957
Ilya Berestnev <berestnev86@gmail.com>
2023-02-12 22:49:11 +0300

    Готовая программа

--- HW/Program.cs
+++ HW/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿Console.Clear();
+int number = UserNumber("Введите количество элементов в массиве: ", "Ошибка ввода!");
+string[] mass = new string[number];
+string temp = "";
+
+for (int i = 0; i < number; i++)
+{
+    temp = UserText($"Введите {i + 1}-й элемент массива: ");
+    mass[i] = temp;
+}
+
+Console.Write("[");
+for (int i = 0; i < number - 1; i++)
+{
+    Console.Write($"\"{mass[i]}\", ");
+}
+Console.Write($"\"{mass[number - 1]}\"] -> [");
+
+for (int i = 0; i < number; i++)
+{
+    if(mass[i].Length <= 3)
+    {
+        Console.Write($"\"{mass[i]}\" ");
+    }
+}
+Console.Write("]");
+
+
+string UserText(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+int UserNumber(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
